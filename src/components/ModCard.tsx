8e2f3472c0ab3c2cf2fb0e17acb4ad656debb8f5
NotGhoull <jay07lew@gmail.com>
2024-10-11 22:54:58 +0100

    feat: Add rendering of mod to the frontend

--- src/components/ModCard.tsx
+++ src/components/ModCard.tsx
@@ -1,4 +1,7 @@
 import { DownloadCloudIcon, HeartIcon, UploadCloudIcon } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
 
 interface ModCardProps {
   thumbnailLink: string;
@@ -13,44 +16,67 @@ interface ModCardProps {
 
 function ModCard(props: ModCardProps) {
   return (
-    <div className="flex flex-row w-full gap-4 p-4 text-white bg-gray-800 rounded-md shadow-lg">
-      {/* Image Section */}
-      <div>
-        <img
-          src={props.thumbnailLink ?? "https://via.placeplachholder.com/100"}
-          width={"100px"}
-          height={"100px"}
-          alt="mod icon"
-          className="rounded-lg"
-        />
-      </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <div className="flex flex-row w-full gap-4 p-4 text-white bg-gray-800 rounded-md shadow-lg cursor-pointer">
+          {/* Image Section */}
+          <div>
+            <img
+              src={
+                props.thumbnailLink
+                  ? `https://storage.modworkshop.net/mods/images/thumbnail_${props.thumbnailLink}`
+                  : "https://via.placeplachholder.com/100"
+              }
+              width={"100px"}
+              height={"100px"}
+              alt="mod icon"
+              className="object-cover rounded-lg aspect-square"
+            />
+          </div>
 
-      {/* Text Section */}
-      <div className="flex flex-col justify-center flex-1">
-        <h1 className="text-xl font-bold">
-          {props.title ?? "No title provided."}
-        </h1>
-        <p className="text-sm text-gray-400">
-          {props.description ?? "No description provided."}
-        </p>
-      </div>
+          {/* Text Section */}
+          <div className="flex flex-col justify-center flex-1">
+            <h1 className="text-xl font-bold">
+              {props.title ?? "No title provided."}
+            </h1>
+            <p className="text-sm text-gray-400 line-clamp-2">
+              {props.description ?? "No description provided."}
+            </p>
+          </div>
 
-      {/* Update Section */}
-      <div className="flex flex-col items-end justify-between">
-        <span className="flex items-center gap-1 text-xs text-gray-500">
-          <DownloadCloudIcon />
-          {props.downloads ?? "?"} Downloads
-        </span>
-        <span className="flex items-center gap-1 text-xs text-gray-500">
-          <HeartIcon />
-          {props.likes ?? "?"} Likes
-        </span>
-        <span className="flex items-center gap-1 text-xs text-gray-500">
-          <UploadCloudIcon />
-          Updated {props.update ?? "?"} days ago
-        </span>
-      </div>
-    </div>
+          {/* Update Section */}
+          <div className="flex flex-col items-end justify-between">
+            <span className="flex items-center gap-1 text-xs text-gray-500">
+              <DownloadCloudIcon />
+              {props.downloads ?? "?"} Downloads
+            </span>
+            <span className="flex items-center gap-1 text-xs text-gray-500">
+              <HeartIcon />
+              {props.likes ?? "?"} Likes
+            </span>
+            <span className="flex items-center gap-1 text-xs text-gray-500">
+              <UploadCloudIcon />
+              Updated {props.update ?? "?"} days ago
+            </span>
+          </div>
+        </div>
+      </PopoverTrigger>
+
+      <PopoverContent className="flex flex-col gap-2 w-96">
+        <div className="flex flex-col gap-0">
+          <h1 className="text-xl font-bold text-foreground">Mod options</h1>
+          <p>Buttons don't work, since this isn't done yet</p>
+        </div>
+        <Separator className="bg-foreground" />
+        <Button disabled>Download mod</Button>
+        <Button disabled variant={"outline"}>
+          Update mod
+        </Button>
+        <Button disabled variant={"destructive"}>
+          Delete mod
+        </Button>
+      </PopoverContent>
+    </Popover>
   );
 }
 
--- src/components/Pages/ModsPage.tsx
+++ src/components/Pages/ModsPage.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import ModCard from "../ModCard";
+import { Alert, AlertTitle } from "../ui/alert";
+import { TriangleAlertIcon } from "lucide-react";
 
 interface Mod {
   id: number;
   name: string;
+  description: string;
   version: number;
   download_url: string;
   installed: boolean;
+  thumbnail: string;
 }
 
 function ModsPage() {
@@ -22,10 +26,13 @@ function ModsPage() {
       let data = await invoke<Mod[]>("fetch_payday2_mods");
 
       // We have to just assume this is going to be the right value, since we don't know yet.
+      console.debug(data);
       setMods(data);
-      setTimeout(() => {}, 4000);
+      // setTimeout(() => {}, 4000);
       setPageState("ready");
     };
+
+    // setPageState("errored");
 
     get_data();
   }, []);
@@ -40,17 +47,44 @@ function ModsPage() {
             </h1>
           </div>
           <div className="flex flex-1 p-8 pt-4 pb-4 border border-dashed rounded-lg shadow-sm">
-            <div className="flex flex-col gap-1">Good things are coming...</div>
+            <div className="flex flex-col items-center justify-center w-full h-full gap-5">
+              <h1 className="text-2xl font-bold">(ノಥ,_｣ಥ)ノ彡┻━┻</h1>
+              <p>
+                Something went wrong when loading mods, please check console for
+                output
+              </p>
+            </div>
           </div>
         </main>
       );
 
     case "loading":
-      return <h1>Loading...</h1>;
+      return (
+        <main className="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
+          <div className="flex items-center">
+            <h1 className="text-lg font-semibold md:text-2xl">
+              Mods for GameName
+            </h1>
+          </div>
+          <div className="flex flex-1 p-8 pt-4 pb-4 border border-dashed rounded-lg shadow-sm">
+            <div className="flex flex-col items-center justify-center w-full h-full gap-5">
+              <h1 className="text-2xl font-bold">The mods are on the way...</h1>
+              <div className={`loader`} />
+            </div>
+          </div>
+        </main>
+      );
   }
 
   return (
     <main className="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
+      <Alert>
+        <TriangleAlertIcon className="z-0 w-4 h-4" />
+        <AlertTitle>
+          Mods on this page do not currently support the version paramter. Do
+          not trust the updater.
+        </AlertTitle>
+      </Alert>
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Mods for PAYDAY 2</h1>
       </div>
@@ -61,8 +95,8 @@ function ModsPage() {
           {mods.map((mod) => (
             <ModCard
               title={mod.name}
-              description="This was created from rust server side!"
-              thumbnailLink="placehold.co/100"
+              description={mod.description}
+              thumbnailLink={mod.thumbnail}
             />
           ))}
         </div>
